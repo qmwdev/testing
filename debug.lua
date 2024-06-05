aba3c886d5fe7dc1d094242005579a41a3f99cf4
qmwdev <qmw.qmw@mail.ru>
2024-06-05 15:54:45 +0500

    Update debug.lua

--- debug.lua
+++ debug.lua
@@ -2,47 +2,43 @@ ver_type, ver = 'debug', '0.1'
 
 
 
-local api = {
-    flower_zones = game.Workspace.FlowerZones,
-    flower_decos = game.Workspace.FieldDecos,
-    collectibles = game.Workspace.Collectibles,
+local api = {}
 
 
-    notify = function(Title, Text, Icon, Duration)
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = Title, -- Required
-            Text = Text, -- Required
-            Icon = Icon or "rbxassetid://1234567890", -- Optional
-            Duration = Duration or 5,
-        })
-    end,
+flower_zones = game.Workspace.FlowerZones
+flower_decos = game.Workspace.FieldDecos
+collectibles = game.Workspace.Collectibles
 
-    fields = function(variable)
-        for i, v in pairs(variable:GetChildren()) do
-            if v:IsA("BasePart") then
-                local field_name = v.Name
-                local field_coords = v.Position
-                print(field_name, ':', field_coords)
-            end
+
+function api:notify(Title, Text, Icon, Duration)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = Title, -- Required
+        Text = Text, -- Required
+        Icon = Icon or "rbxassetid://1234567890", -- Optional
+        Duration = Duration or 5,
+    })
+end
+
+function api:fields(variable)
+    for i, v in pairs(variable:GetChildren()) do
+        if v:IsA("BasePart") then
+            local field_name = v.Name
+            local field_coords = v.Position
+            print(field_name, ':', field_coords)
         end
-    end,
-
-    collect_collectibles = function(variable)
-        api.notify('Started','Started')
-        for i, v in pairs(variable:GetChildren()) do
-            if v:IsA("BasePart") then
-                wait(3)
-                game.Players.LocalPlayer.Character:MoveTo(v.Position)
-            end
-        end
-        
     end
+end
 
-
-}
-
-
-
+function api:collect_collectibles(variable)
+    api.notify('Started','Started')
+    for i, v in pairs(variable:GetChildren()) do
+        if v:IsA("BasePart") then
+            wait(3)
+            game.Players.LocalPlayer.Character:MoveTo(v.Position)
+        end
+    end
+    
+end
 
 
 --collect_collectibles(api.collectibles)
