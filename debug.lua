2d5de6a124770147e5e50867a08ae00c5b91f567
qmwdev <qmw.qmw@mail.ru>
2024-06-05 15:34:44 +0500

    Update debug.lua

--- debug.lua
+++ debug.lua
@@ -15,33 +15,35 @@ local api = {
             Icon = Icon or "rbxassetid://1234567890", -- Optional
             Duration = Duration or 5,
         })
+    end,
+
+    fields = function(variable)
+        for i, v in pairs(variable:GetChildren()) do
+            if v:IsA("BasePart") then
+                local field_name = v.Name
+                local field_coords = v.Position
+                print(field_name, ':', field_coords)
+            end
+        end
+    end,
+
+    collect_collectibles = function(variable)
+        api.notify('Started','Started')
+        for i, v in pairs(variable:GetChildren()) do
+            if v:IsA("BasePart") then
+                wait(3)
+                game.Players.LocalPlayer.Character:MoveTo(v.Position)
+            end
+        end
+        
     end
-
-
 
 
 }
 
-local fields = function(variable)
-    for i, v in pairs(variable:GetChildren()) do
-        if v:IsA("BasePart") then
-            local field_name = v.Name
-            local field_coords = v.Position
-            print(field_name, ':', field_coords)
-        end
-    end
-end
 
-local collect_collectibles = function(variable)
-    api.notify('Started','Started')
-    for i, v in pairs(variable:GetChildren()) do
-        if v:IsA("BasePart") then
-            wait(3)
-            game.Players.LocalPlayer.Character:MoveTo(v.Position)
-        end
-    end
-    
-end
+
+
 
 --collect_collectibles(api.collectibles)
 --fields(api.flower_zones)
